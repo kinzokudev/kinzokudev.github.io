4e95ff76858be344cc56abdaecae68228b2f6f7c
Kira Rene <kira@renec88.xyz>
2025-04-10 02:07:09 -0400

    chore(config): add some astro plugins
    Signed-off-by: Kira Rene <kira@renec88.xyz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kinzoku.dev",
+  integrations: [mdx(), sitemap(), icon()],
+  markdown: {
+    syntaxHighlight: "prism",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
